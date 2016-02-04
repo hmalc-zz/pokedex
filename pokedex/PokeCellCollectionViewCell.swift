29d5dee5e07a5eb1a4bbbaaad0b3e25cfa2dae8c
hmalc <hayden.malcomson@me.com>
2016-02-04 12:38:36 -0700

    Added storyboard and PokeCell class

---
+++ pokedex/PokeCellCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  pokedex
+//
+//  Created by Hayden Malcomson on 2016-02-04.
+//  Copyright © 2016 Hayden Malcomson. All rights reserved.
+//
+
+import UIKit
+
+class PokeCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Hayden Malcomson on 2016-02-04.
+//  Copyright © 2016 Hayden Malcomson. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+}
