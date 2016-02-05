dacdfd941eb655502e068b4f7b793f9d912ca03c
hmalc <hayden.malcomson@me.com>
2016-02-04 18:43:44 -0700

    Added status page

---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,53 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by Hayden Malcomson on 2016-02-04.
+//  Copyright © 2016 Hayden Malcomson. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    var pokemon: Pokemon!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var currentEvo: UIImageView!
+    @IBOutlet weak var nextEvo: UIImageView!
+    @IBOutlet weak var evoLabel: NSLayoutConstraint!
+    
+    
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.text = pokemon.name
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backToMain(sender: AnyObject) {
+    
+        dismissViewControllerAnimated(true, completion: nil)
+    
+    }
+    
+
+}
